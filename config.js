0ab4d80d248245ca26cbeb817335b537c049e73e
Chimmy2004 <bella.ash1997@gmail.com>
2026-02-15 02:01:13 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@
 const CONFIG = {
     // Your Valentine's name that will appear in the title
     // Example: "Jade", "Sarah", "Mike"
-    valentineName: "Agustin Lopez",
+    valentineName: "Elton de oliveira dos santos",
 
     // The title that appears in the browser tab
     // You can use emojis! 💝 💖 💗 💓 💞 💕
